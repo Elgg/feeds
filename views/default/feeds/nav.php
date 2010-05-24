fbd4b60660147e5e01e3a5be845f1e05a5367c43
Pete Harris <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-24 14:34:30 +0000

    UI updates to Feeds sidebar contents.
    git-svn-id: http://code.elgg.org@6169 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/feeds/nav.php
+++ views/default/feeds/nav.php
@@ -31,4 +31,4 @@ switch($vars['filter']) {
 			}
 		?>
 	</ul>
-</div><br />
+</div>
--- views/default/feeds/sidebar.php
+++ views/default/feeds/sidebar.php
@@ -2,7 +2,7 @@
 $count = $vars['count'];
 $context = $vars['context'];
 
-echo "<h2>Feeds stats</h2>";
+echo "<h3>Feeds stats</h3>";
 
 if($context == 'all')
 	echo "So far, " . $count . " feeds have been added to the site feed reader.";
