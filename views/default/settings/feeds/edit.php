6a803529883bd55bd64a52db76e5de59df98b12a
Pete Harris <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-10 21:41:21 +0000

    Updated feeds admin form layout
    git-svn-id: http://code.elgg.org@6450 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/settings/feeds/edit.php
+++ views/default/settings/feeds/edit.php
@@ -6,41 +6,41 @@ if (!$limit) {
 	$limit = 10;
 }
 
-$body .= elgg_echo('feeds:settings:limit:title');
+$body .= "<p>".elgg_echo('feeds:settings:limit:title');
 $body .= '<br />';
 $body .= elgg_view('input/text',array('internalname'=>'params[limit]','value'=>$limit));
 
-$body .= '<br />';
+$body .= '</p>';
 
 $cache_duration = get_plugin_setting('cache_duration', 'feeds');
 if (!$cache_duration) {
 	$cache_duration = 600;
 }
 
-$body .= elgg_echo('feeds:settings:cache_duration:title');
+$body .= "<p>".elgg_echo('feeds:settings:cache_duration:title');
 $body .= '<br />';
 $body .= elgg_view('input/text',array('internalname'=>'params[cache_duration]','value'=>$cache_duration));
 
-$body .= '<br />';
+$body .= '</p>';
 
 $throttle = get_plugin_setting('throttle', 'feeds');
 if (!$throttle) {
 	$throttle = 0;
 }
 
-$body .= elgg_echo('feeds:settings:throttle:title');
+$body .= "<p>".elgg_echo('feeds:settings:throttle:title');
 $body .= '<br />';
 $body .= elgg_view('input/text',array('internalname'=>'params[throttle]','value'=>$throttle));
 
-$body .= '<br />';
+$body .= '</p>';
 
 $date_format = get_plugin_setting('date_format', 'feeds');
 if (!$date_format) {
 	$date_format = 'F jS, Y';
 }
 
-$body .= elgg_echo('feeds:settings:date_format:title');
+$body .= "<p>".elgg_echo('feeds:settings:date_format:title');
 $body .= '<br />';
 $body .= elgg_view('input/text',array('internalname'=>'params[date_format]','value'=>$date_format));
-
+$body .= '</p>';
 echo $body;
